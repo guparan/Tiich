4377b9ef1a20618f37032afa311c2763fca172af
Scoms <wieser.laurent@gmail.com>
2014-11-03 15:41:19 +0100

    Socle architecturale

---
+++ Utils/ErrorHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class ErrorHandler
+    {
+        private string _errors;
+        private bool _hasErrors;
+
+        public ErrorHandler()
+        {
+            this._errors = String.Empty;
+            this._hasErrors = false;
+        }
+
+        public void addError(string error)
+        {
+            this._errors += error + ". ";
+            this._hasErrors = true;
+        }
+
+        public bool hasErrors()
+        {
+            return this._hasErrors;
+        }
+
+        public string getErrors()
+        {
+            return this._errors;
+        }
+    }
+}
