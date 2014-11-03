b82ce5c059d88e52e8ae35525062775bc91ad989
Scoms <wieser.laurent@gmail.com>
2014-11-03 14:40:31 +0100

    Initial commit

---
+++ Tiich/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tiich
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
