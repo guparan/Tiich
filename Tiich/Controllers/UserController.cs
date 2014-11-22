207324e56d0267af280809ee1d2784844e12f479
Scoms <wieser.laurent@gmail.com>
2014-11-22 13:26:55 +0100

    MAJ.

--- Tiich/Controllers/UserController.cs
+++ Tiich/Controllers/UserController.cs
@@ -69,98 +69,17 @@ namespace Tiich.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        //
-        // GET: /User/
-
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Account(int id = -1)
         {
-            return View(new User());
-        }
+            UserService service = new UserService();
+            User user = new User();
+            if (User.Identity.IsAuthenticated)
+                user = service.GetUserByName(User.Identity.Name);
+            else
+                user = null;
 
-        //
-        // GET: /User/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /User/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /User/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /User/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /User/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /User/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /User/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return View(user);
         }
     }
 }
