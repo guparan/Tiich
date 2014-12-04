6c9210da72f0bbfe91e51b77dcafc1296ca4b58c
Scoms <wieser.laurent@gmail.com>
2014-12-04 14:34:21 +0100

    terrain préparé pour les appels Ajax sur les vues partielles.

---
+++ Tiich/Controllers/ModerationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tiich.Controllers
+{
+    public class ModerationController : Controller
+    {
+        //
+        // GET: /Moderation/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialAjaxTest(string text)
+        {
+            return View("PartialAjaxTest", text);
+        }
+
+        public ActionResult GlobalTagsStatistics()
+        {
+            return View();
+        }
+    }
+}
